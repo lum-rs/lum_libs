24cbab6e7e218bbdf293a4e58f8dc36f761ff175
Torben Schweren <torben@schweren.dev>
2025-05-30 00:59:10 +0200

    add: cargo dependency feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,38 @@
+#[cfg(feature = "async-trait")]
 pub use async_trait;
+
+#[cfg(feature = "dirs")]
 pub use dirs;
+
+#[cfg(feature = "downcast-rs")]
 pub use downcast_rs;
+
+#[cfg(feature = "fern")]
 pub use fern;
+
+#[cfg(feature = "humantime")]
 pub use humantime;
+
+#[cfg(feature = "log")]
 pub use log;
+
+#[cfg(feature = "rustls")]
 pub use rustls;
+
+#[cfg(feature = "serde")]
 pub use serde;
+
+#[cfg(feature = "serde-env")]
 pub use serde_env;
+
+#[cfg(feature = "serde_json")]
 pub use serde_json;
+
+#[cfg(feature = "serenity")]
 pub use serenity;
+
+#[cfg(feature = "tokio")]
 pub use tokio;
+
+#[cfg(feature = "uuid")]
 pub use uuid;
